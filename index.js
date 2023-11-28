74719b2ac2dab8f9b04fc3bcd8d208cb4e1ed956
Freelancerhatem <108505344+Freelancerhatem@users.noreply.github.com>
2023-11-28 09:02:55 +0600

    parcel stattus updated

--- index.js
+++ index.js
@@ -20,18 +20,52 @@ const client = new MongoClient(uri, {
 async function run(){
     try{
         const userCollection = client.db("parcelDB").collection('users');
+        const reviewsCollection = client.db("parcelDB").collection('reviews');
+        const dmanCollection = client.db("parcelDB").collection('deliveryMan');
+        const parcelCollection = client.db("parcelDB").collection('allparcel');
 
         app.get('/users',async(req,res)=>{
             const result = await userCollection.find().toArray();
             res.send(result);
         });
-        app.patch('/users/admin/:id',async(req,res)=>{
+        app.patch('/users/deliveryman/:id',async(req,res)=>{
             const id = req.params.id;
             const query = {_id:new ObjectId(id)}
             const data = {userType: 'deliveryMan'};
             const result = await userCollection.updateOne(query,{$set: data });
             res.send(result);
 
+        })
+        app.patch('/users/admin/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:new ObjectId(id)}
+            const data = {userType: 'admin'};
+            const result = await userCollection.updateOne(query,{$set: data });
+            res.send(result);
+
+        });
+        app.get('/reviews',async(req,res)=>{
+            const result = await reviewsCollection.find().toArray();
+            res.send(result); 
+        });
+        app.get('/deliveryman',async(req,res)=>{
+            const result = await dmanCollection.find().toArray();
+            res.send(result);
+        });
+        app.get('/allparcel',async(req,res)=>{
+            const result = await parcelCollection.find().toArray();
+            res.send(result);
+        })
+        app.patch('/parcelstatus',async(req,res)=>{
+            const data = req.body;
+            // console.log(data);
+            const id = data.parcelId;
+            const filter = {_id:new ObjectId(id)};
+            const deliveryManId = data.id
+            const query = {$set:{status:data.status,deliveryman:deliveryManId}}
+            const options = { upsert: true };
+            const update = await parcelCollection.updateOne(filter,query,options);
+            res.send(update);
         })
     }
     finally{
